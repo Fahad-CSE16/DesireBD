204f60bc49074d462451449404ec9f610e1375c6
Fahad-CSE16 <Fahad-CSE16>
2020-09-14 19:39:57 +0600

    djano suit used for admin pannel, some added to userprofile model

--- person/admin.py
+++ person/admin.py
@@ -3,7 +3,9 @@ from . models import UserProfile, Contact
 from . models import SSC,HSC
 from . models import HigherStudies,AfterHsc
 from . models import TuitionClass,District,SubDistrict,Subject,Classes
-
+from django.urls import path
+from django.http import HttpResponseRedirect
+from django.utils.html import format_html
 
 class HSCInline(admin.TabularInline):
     model = HSC
@@ -38,9 +40,35 @@ class DistrictAdmin(admin.ModelAdmin):
     inlines = [
         SubDistrictInline,
     ]
+admin.site.site_header = 'DesireBD Admin Panel'
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    # fields = ('user', 'blood_group')
+    # exclude = ('user', 'blood_group')
+    list_display = ('user', 'blood_group', 'genre',
+                    'marital_status', 'nationality_html_display', 'profession')
+    list_filter = ('blood_group', 'genre', 'marital_status', 'profession')
+    change_list_template = 'admin/userprofile/userprofile_change_list.html'
+    readonly_fields=('address',)
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('fontsize/<str:slug>/', self.change_nationality)
+        ]
+        return custom_urls+ urls
+    def change_nationality(self, request, slug):
+        self.model.objects.all().update(nationality=slug)
+        return HttpResponseRedirect("/admin/person/userprofile/")
+    def nationality_html_display(self, obj):
+        return format_html(
+            f'<span style="font-size: 20px; color:blue;"> {obj.nationality}</span> '
+        )
+    
+    
 # Register your models here.
 admin.site.register(Contact)
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(SSC)
 admin.site.register(HSC)
 admin.site.register(AfterHsc)
